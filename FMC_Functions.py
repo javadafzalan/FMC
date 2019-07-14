1d8dbc4dae643a29bcf295a7f0771280e80d9f9d
javadafzalan <35040418+javadafzalan@users.noreply.github.com>
2019-07-14 08:01:01 -0700

    Update FMC_Functions.py
    add acp rule detail function

--- FMC_Functions.py
+++ FMC_Functions.py
@@ -86,15 +86,68 @@ def GET_ACCESSRULE_LIST(IP,USERNAME,PASSWORD,ACCESS_POLICY_ID):
         if r : r.close()
     return json_resp
 ##########################################################################################################################
+def GET_ACCESSRULE_DETAIL(IP,USERNAME,PASSWORD,ACCESS_POLICY_ID,RULE_ID):
+    import json
+    import sys
+    import requests
+    server = "https://"+IP
+    username = USERNAME
+    password = PASSWORD
+    ACP_ID=ACCESS_POLICY_ID
+    r = None
+    headers = {'Content-Type': 'application/json'}
+    api_auth_path = "/api/fmc_platform/v1/auth/generatetoken"
+    auth_url = server + api_auth_path
+    try:
+        r = requests.post(auth_url, headers=headers, auth=requests.auth.HTTPBasicAuth(username,password), verify=False)
+        auth_headers = r.headers
+        auth_token = auth_headers.get('X-auth-access-token', default=None)
+        if auth_token == None:
+            print("auth_token not found. Exiting...")
+            sys.exit()
+    except Exception as err:
+        print ("Error in generating auth token --> "+str(err))
+        sys.exit()
+    headers['X-auth-access-token']=auth_token
+    api_path = "/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/policy/accesspolicies/"+ACP_ID+"/accessrules/"+RULE_ID# param
+    url = server + api_path
+    if (url[-1] == '/'):
+        url = url[:-1]
+    # GET OPERATION
+    try:
+        r = requests.get(url, headers=headers, verify=False)
+        status_code = r.status_code
+        resp = r.text
+        if (status_code == 200):
+            #print("GET successful. Response data --> ")
+            json_resp = json.loads(resp)
+            print(json_resp)
+        else:
+            r.raise_for_status()
+            print("Error occurred in GET --> "+resp)
+    except requests.exceptions.HTTPError as err:
+        print ("Error in connection --> "+str(err)) 
+    finally:
+        if r : r.close()
+    return json_resp
+
 
 ## Main program  ##
 import json
-list1=GET_ACP_LIST("192.168.1.48","javad","P@ssw0rd")
-#acp_id=list1[1]['id']
+SERVER_IP="192.168.1.48"
+USERNAME="javad"
+PASSWORD="P@ssw0rd"
+#get list of acp
+list1=GET_ACP_LIST(SERVER_IP,USERNAME,PASSWORD)
+ACP_ID=list1[1]['id']
 
-
-#ACL_LIST=GET_ACCESSRULE_LIST("192.168.1.48","javad","P@ssw0rd",acp_id)
-##print(json.dumps(ACL_LIST,sort_keys=True,indent=4, separators=(',', ': ')))
-#for item in ACL_LIST['items']:
-#    print(item['name']+" ::: "+item['links']['self'] )
-
+#get list of accessrules in acp
+ACL_LIST=GET_ACCESSRULE_LIST(SERVER_IP,USERNAME,PASSWORD,ACP_ID)
+#print(json.dumps(ACL_LIST,sort_keys=True,indent=4, separators=(',', ': ')))
+for item in ACL_LIST['items']:
+    RULE_ID=str(item['id'])
+    print("================="+item['name']+"=================")
+    #get access rule details
+    #print(GET_ACCESSRULE_DETAIL(SERVER_IP,USERNAME,PASSWORD,ACP_ID,RULE_ID))
+    print(json.dumps(GET_ACCESSRULE_DETAIL(SERVER_IP,USERNAME,PASSWORD,ACP_ID,RULE_ID),sort_keys=True,indent=4, separators=(',', ': ')))
+    print("==================================================================================")
