6d343f4075f5f00c54df7aecfd4b9cf6e9b9354e
Javad Afzalan <J.Afzalan@PazhNetwork.com>
2019-07-17 11:55:58 -0700

    contrast main from functions

--- FMC_Functions.py
+++ FMC_Functions.py
@@ -111,24 +111,3 @@ def GET_LOGIN_TOKEN(IP,USERNAME,PASSWORD):
         sys.exit()
     return auth_token
 #############################################################################################
-
-## Main program  ##
-import json
-SERVER_IP="192.168.1.48"
-USERNAME="javad"
-PASSWORD="P@ssw0rd"
-#get list of acp
-token=GET_LOGIN_TOKEN(SERVER_IP,USERNAME,PASSWORD)
-
-list1=GET_ACP_LIST(SERVER_IP,token)
-ACP_ID=list1[1]['id']
-
-#get list of accessrules in acp
-ACL_LIST=GET_ACCESSRULE_LIST(SERVER_IP,token,ACP_ID)
-for item in ACL_LIST['items']:
-    RULE_ID=str(item['id'])
-    print("================="+item['name']+"=================")
-    #get access rule details
-    print(GET_ACCESSRULE_DETAIL(SERVER_IP,token,ACP_ID,RULE_ID))
-    #print(json.dumps(GET_ACCESSRULE_DETAIL(SERVER_IP,token,ACP_ID,RULE_ID),sort_keys=True,indent=4, separators=(',', ': ')))
-    print("==================================================================================")
